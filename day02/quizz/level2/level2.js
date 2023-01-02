a2d535e5da19ca1c7e0c0023af281a8b8bd525ee
nasilKiM <nasikun@naver.com>
2023-01-02 15:38:04 +0900

    2번 한글설계 수정

--- day02/quizz/level2/level2.js
+++ day02/quizz/level2/level2.js
@@ -25,32 +25,14 @@ console.log(RESERVATION_LIST);
  (3) 입력 후 input창을 초기화 시키는 function
  
 3. 한글로 함수 설계하기
- (1) 입력한 데이터가 RESERVATION_List에 있는 배열에 포함되어 있는지 찾는다. 
-    .name && .phone-number 
- (3) 일치하는지 확인한다.
- (4) 일치하면 예약번호가 리스트 형태로 나타난다.
- (4-1) 일치하지 않으면 팝업으로 안내한다.
+ (1) '예약번호 확인' 버튼 클릭 함수
+   -  입력한 데이터가 RESERVATION_List에 있는 배열에 포함되어 있는지 찾는다. // .name && .phone-number 
+ (2) 일치하는지 확인한다.
+ (3) 일치하면 예약번호가 리스트 형태로 나타난다.
+ (3-1) 일치하지 않으면 팝업으로 안내한다.
 
 4. 구글링 키워드
  - 배열에 값 포함 되어있는지 찾는 기능
- - ㅈ
+ - 
 
-*/
-
-/*
-3. input을 토대로 기술 검증
-(1) 이미지를 클릭하면 이미지 태그가 display none이 된다.
-// javascript 요소 display none
-
-(2) 이미지를 클릭하면 이미지와 겹쳐있던 vidoe 태그가 실행된다.
-// javascript video 실행
-
-4. 조건
-(1) 이미지 태그와 video 태그를 겹치는 것
-    position: absolute
-    z-index
-
-(2) 이미지 클릭 함수 
-    가. 이미지 클릭시 이미지 displaynone
-    나. video 실행
 */
