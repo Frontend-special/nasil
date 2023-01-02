702a2125799a5240ab85118694283c329a284fad
nasilKiM <nasikun@naver.com>
2023-01-03 00:51:01 +0900

    구글링 키워드 + 기능 분석 추가

--- day02/answer/level3/level3.js
+++ day02/answer/level3/level3.js
@@ -42,6 +42,7 @@ $form.addEventListener('submit', (e) => {
   $weight.value = '';
 });
 
+// 제출 클릭 시, 현재 테이블 전체 값 추출
 $button.addEventListener('click', () => {
   if ($list.children.length > 0) $list.innerHTML = '';
   INGREDIENT_LIST.forEach((weight, ingredient) => {
@@ -58,10 +59,11 @@ $button.addEventListener('click', () => {
   1. input창 초기화
   2. td 테이블 행 추가
   3. input value 와 테이블 데이터 간 중복 확인
-  4. 테이블 값 추출해서 리스트로 변환
+  4. 테이블 행 값 추출
   
 * 강사님은 왜 이기능을 썼을까? 목적 결과값
   .set
   .append
+  .for each
 
 */
